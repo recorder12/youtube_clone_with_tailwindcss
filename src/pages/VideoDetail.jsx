da971fbd6930cf0d071c7dddc731abc92614868c
Byeonggeon Kim <spinkbg@gmail.com>
2023-05-13 23:40:11 -0400

    init

---
+++ src/pages/VideoDetail.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import ChannelInfo from '../components/ChannelInfo';
+import RelatedVideos from '../components/RelatedVideos';
+
+export default function VideoDetail() {
+  const {state : {video}} = useLocation();
+  const {title, channelId, channelTitle, description} = video.snippet;  
+  console.log(video);
+  return <section className='flex flex-col lg:flex-row'>
+    <article className='basis-4/6'>
+      <iframe 
+        width="100%" 
+        height="640" 
+        src={`https://www.youtube.com/embed/${video.id}`} 
+        title={title} 
+        frameBorder="0"/>
+        <div className='p-8'>
+          <h2 className='text-xl font-bold'>{title}</h2>
+          <ChannelInfo id={channelId} name={channelTitle}/>
+          <pre className='whitespace-pre-wrap'>{description}</pre>
+        </div>
+    </article>
+    <section className='basis-2/6'>
+      <RelatedVideos id={video.id} />
+    </section>
+  </section>;
+}
